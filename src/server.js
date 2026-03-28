c1b725fb0373e6c1f70f5609a4b28cee82c506f0
Manuela Camara <camarajoice01@gmail.com>
2026-03-28 19:05:05 -0300

    fix: ajusta porta e configura pasta public para o render

--- src/server.js
+++ src/server.js
@@ -1,13 +1,27 @@
 import app from './app.js';
 import open from 'open';
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-const PORT = 5000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, '../public')));
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, async () => {
   const url = `http://localhost:${PORT}`;
 
-  console.log('🚀 Servidor rodando!');
-  console.log(`👉 ${url}`);
+  console.log(`🚀 Servidor rodando na porta ${PORT}!`);
 
-  await open(url);
+  if (!process.env.PORT) {
+    try {
+      console.log(`👉 ${url}`);
+      await open(url);
+    } catch (error) {
+      console.log('Não foi possível abrir o navegador automaticamente.');
+    }
+  }
 });
